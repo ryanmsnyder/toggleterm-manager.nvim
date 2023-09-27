1ac2307bec9c8035a3b709c3ac292aace9cfc072
Ryan Snyder <ryansnyder4@gmail.com>
2023-09-26 20:27:49 -0600

    feat: add previewer and display toggleterm display_name if available; otherwise, display toggle_number

---
+++ lua/lib/displayer/init.lua
@@ -0,0 +1,103 @@
+local entry_display = require("telescope.pickers.entry_display")
+local utils = require("telescope.utils")
+local strings = require("plenary.strings")
+local Path = require("plenary.path")
+local make_entry = require("telescope.make_entry")
+
+local M = {}
+
+function M.gen_displayer(opts)
+	opts = opts or {}
+
+	local disable_devicons = opts.disable_devicons
+
+	local icon_width = 0
+	if not disable_devicons then
+		local icon, _ = utils.get_devicons("fname", disable_devicons)
+		icon_width = strings.strdisplaywidth(icon)
+	end
+
+	local displayer = entry_display.create({
+		separator = " ",
+		items = {
+			-- { width = opts.bufnr_width },
+			{ width = 4 },
+			{ width = icon_width },
+			{ remaining = true },
+		},
+	})
+
+	local cwd = vim.fn.expand(opts.cwd or vim.loop.cwd())
+
+	local make_display = function(entry)
+		-- bufnr_width + modes + icon + 3 spaces + : + lnum
+		-- opts.__prefix = opts.bufnr_width + 4 + icon_width + 3 + 1 + #tostring(entry.lnum)
+		opts.__prefix = opts.bufnr_width + 4 + icon_width + 3 + 1
+		local display_bufname = utils.transform_path(opts, entry.filename)
+		local icon, hl_group = utils.get_devicons(".terminal", disable_devicons)
+
+		-- local term_number = vim.api.nvim_buf_get_var(entry.bufnr, "toggle_number")
+		-- local display_name = require("toggleterm.terminal").get(term_number, false).display_name
+		-- local term_name = display_name or term_number -- number icon if id is less than 11
+
+		return displayer({
+			-- { entry.bufnr, "TelescopeResultsNumber" },
+			-- { display_bufname, "TelescopeResultsNumber" },
+			-- { term_name, "TelescopeResultsNumber" },
+			{ entry.indicator, "TelescopeResultsComment" },
+			{ icon, hl_group },
+			entry.ordinal, -- term_name
+			-- display_bufname
+			-- 	.. ":"
+			-- 	.. entry.lnum,
+			-- entry.filename,
+		})
+	end
+
+	return function(entry)
+		local bufname = entry.info.name ~= "" and entry.info.name or "[No Name]"
+		-- if bufname is inside the cwd, trim that part of the string
+		bufname = Path:new(bufname):normalize(cwd)
+
+		local hidden = entry.info.hidden == 1 and "h" or "a"
+		local readonly = vim.api.nvim_buf_get_option(entry.bufnr, "readonly") and "=" or " "
+		local changed = entry.info.changed == 1 and "+" or " "
+		local indicator = entry.flag .. hidden .. readonly .. changed
+		local lnum = 1
+
+		-- account for potentially stale lnum as getbufinfo might not be updated or from resuming buffers picker
+		if entry.info.lnum ~= 0 then
+			-- but make sure the buffer is loaded, otherwise line_count is 0
+			if vim.api.nvim_buf_is_loaded(entry.bufnr) then
+				local line_count = vim.api.nvim_buf_line_count(entry.bufnr)
+				lnum = math.max(math.min(entry.info.lnum, line_count), 1)
+			else
+				lnum = entry.info.lnum
+			end
+		end
+
+		-- DELETE
+		local desktop_path = "/Users/ryan.snyder/Desktop/debug.txt"
+		-- Open the debug file in append mode
+		local file = io.open(desktop_path, "a")
+		-- Write the debug message to the file
+		file:write(entry.term_name .. "\n")
+		-- Close the file
+		file:close()
+		-- DELETE
+
+		return make_entry.set_default_entry_mt({
+			-- value = bufname,
+			value = entry,
+			-- ordinal = entry.bufnr .. " : " .. bufname,
+			ordinal = entry.term_name, -- for filtering
+			display = make_display,
+
+			bufnr = entry.bufnr,
+			filename = bufname,
+			lnum = lnum,
+			indicator = indicator,
+		}, opts)
+	end
+end
+return M
--- lua/lib/telescope/init.lua
+++ lua/lib/telescope/init.lua
@@ -1,97 +1,127 @@
-local pickers, finders, actions, actions_state, conf
+local pickers, finders, make_entry, actions, actions_state, conf
 if pcall(require, "telescope") then
-   pickers = require "telescope.pickers"
-   finders = require "telescope.finders"
-   actions = require "telescope.actions"
-   actions_state = require "telescope.actions.state"
-   conf = require("telescope.config").values
+	pickers = require("telescope.pickers")
+	finders = require("telescope.finders")
+	make_entry = require("telescope.make_entry")
+	actions = require("telescope.actions")
+	actions_state = require("telescope.actions.state")
+	conf = require("telescope.config").values
 else
-   error "Cannot find telescope!"
+	error("Cannot find telescope!")
 end
 local status_ok, _ = pcall(require, "toggleterm")
 if not status_ok then
-   error "Cannot find toggleterm!"
+	error("Cannot find toggleterm!")
 end
+---
+
+local displayer = require("lib.displayer").gen_displayer
+---
 
 local M = {}
 M.open = function(opts)
-   local bufnrs = vim.tbl_filter(function(b)
-      return vim.api.nvim_buf_get_option(b, "filetype") == "toggleterm"
-   end, vim.api.nvim_list_bufs())
-   -- ╭────────────────────────────────────────────────────────────────────╮
-   -- │                                note                                │
-   -- ╰────────────────────────────────────────────────────────────────────╯
-   -- uncommenting this prevents
-   -- telescope from opening a modal windows when there are
-   -- no terminal buffers open.
-   -- ──────────────────────────────────────────────────────────────────────
-   -- if not next(bufnrs) then
-   --    return
-   -- end
-   -- ──────────────────────────────────────────────────────────────────────
-   table.sort(bufnrs, function(a, b)
-      return vim.fn.getbufinfo(a)[1].lastused > vim.fn.getbufinfo(b)[1].lastused
-   end)
-   local buffers = {}
-   for _, bufnr in ipairs(bufnrs) do
-      local info = vim.fn.getbufinfo(bufnr)[1]
-      local element = {
-         bufnr = info.bufnr,
-         changed = info.changed,
-         changedtick = info.changedtick,
-         hidden = info.hidden,
-         lastused = info.lastused,
-         linecount = info.linecount,
-         listed = info.listed,
-         lnum = info.lnum,
-         loaded = info.loaded,
-         name = info.name,
-         windows = info.windows,
-         terminal_job_id = info.variables.terminal_job_id,
-         terminal_job_pid = info.variables.terminal_job_pid,
-         toggle_number = info.variables.toggle_number,
-      }
-      table.insert(buffers, element)
-   end
-   -- ──────────────────────────────────────────────────────────────────────
-   pickers.new(opts, {
-      prompt_title = "Terminal Buffers",
-      finder = finders.new_table {
-         results = buffers,
-         entry_maker = function(entry)
-            return {
-               value = entry,
-               text = tostring(entry.bufnr),
-               display = tostring(entry.name),
-               ordinal = tostring(entry.bufnr),
-            }
-         end,
-      },
-      sorter = conf.generic_sorter(opts),
-      attach_mappings = function(prompt_bufnr, map)
-         actions.select_default:replace(function()
-            local selection = actions_state.get_selected_entry()
-            if selection == nil then
-               return
-            end
-            local bufnr = tostring(selection.value.bufnr)
-            local toggle_number = selection.value.toggle_number
-            require("toggleterm").toggle_command(bufnr, toggle_number)
-            vim.defer_fn(function()
-               vim.cmd "stopinsert"
-            end, 0)
-         end)
-         -- ╭────────────────────────────────────────────────────────────────────╮
-         -- │                           setup mappings                           │
-         -- ╰────────────────────────────────────────────────────────────────────╯
-         local mappings = require("config").options.telescope_mappings
-         for keybind, action in pairs(mappings) do
-            map("i", keybind, function()
-               action(prompt_bufnr)
-            end)
-         end
-         return true
-      end,
-   }):find()
+	local bufnrs = vim.tbl_filter(function(b)
+		return vim.api.nvim_buf_get_option(b, "filetype") == "toggleterm"
+	end, vim.api.nvim_list_bufs())
+	-- ╭────────────────────────────────────────────────────────────────────╮
+	-- │                                note                                │
+	-- ╰────────────────────────────────────────────────────────────────────╯
+	-- uncommenting this prevents
+	-- telescope from opening a modal windows when there are
+	-- no terminal buffers open.
+	-- ──────────────────────────────────────────────────────────────────────
+	if not next(bufnrs) then
+		print("no terminal buffers are opened/hidden")
+		return
+	end
+	-- ──────────────────────────────────────────────────────────────────────
+	table.sort(bufnrs, function(a, b)
+		return vim.fn.getbufinfo(a)[1].lastused > vim.fn.getbufinfo(b)[1].lastused
+	end)
+	local buffers = {}
+	local toggleterm_name_lengths = {}
+	for _, bufnr in ipairs(bufnrs) do
+		local info = vim.fn.getbufinfo(bufnr)[1]
+		local term_number = vim.api.nvim_buf_get_var(info.bufnr, "toggle_number")
+		local display_name = require("toggleterm.terminal").get(term_number, false).display_name
+		local term_name = display_name or tostring(term_number) -- number icon if id is less than 11
+
+		table.insert(toggleterm_name_lengths, #term_name)
+
+		local flag = (bufnr == vim.fn.bufnr("") and "%") or (bufnr == vim.fn.bufnr("#") and "#" or " ")
+
+		local element = {
+			bufnr = bufnr,
+			flag = flag,
+			-- term_name = term_name,
+			-- changed = info.changed,
+			-- changedtick = info.changedtick,
+			-- hidden = info.hidden,
+			-- lastused = info.lastused,
+			-- linecount = info.linecount,
+			-- listed = info.listed,
+			-- lnum = info.lnum,
+			-- loaded = info.loaded,
+			-- name = info.name,
+			-- name = term_name,
+			term_name = term_name,
+			info = info,
+			-- windows = info.windows,
+			-- terminal_job_id = info.variables.terminal_job_id,
+			-- terminal_job_pid = info.variables.terminal_job_pid,
+			-- toggle_number = info.variables.toggle_number,
+		}
+		table.insert(buffers, element)
+	end
+
+	local entry_maker_opts = {}
+	local max_toggleterm_name_length = math.max(unpack(toggleterm_name_lengths))
+	-- local max_bufnr = math.max(bufnrs)
+	-- entry_maker_opts.bufnr_width = #tostring(max_bufnr)
+	entry_maker_opts.bufnr_width = max_toggleterm_name_length
+	-- ──────────────────────────────────────────────────────────────────────
+	pickers
+		.new(opts, {
+			prompt_title = "Terminal Buffers",
+			previewer = conf.grep_previewer(opts),
+			finder = finders.new_table({
+				results = buffers,
+				entry_maker = displayer(entry_maker_opts),
+				-- entry_maker = function(entry)
+				-- 	return {
+				-- 		value = entry,
+				-- 		text = tostring(entry.bufnr),
+				-- 		display = tostring(entry.name),
+				-- 		ordinal = tostring(entry.bufnr),
+				-- 	}
+				-- end,
+			}),
+			sorter = conf.generic_sorter(opts),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(function()
+					local selection = actions_state.get_selected_entry()
+					if selection == nil then
+						return
+					end
+					local bufnr = tostring(selection.value.bufnr)
+					local toggle_number = selection.value.toggle_number
+					require("toggleterm").toggle_command(bufnr, toggle_number)
+					vim.defer_fn(function()
+						vim.cmd("stopinsert")
+					end, 0)
+				end)
+				-- ╭────────────────────────────────────────────────────────────────────╮
+				-- │                           setup mappings                           │
+				-- ╰────────────────────────────────────────────────────────────────────╯
+				local mappings = require("config").options.telescope_mappings
+				for keybind, action in pairs(mappings) do
+					map("i", keybind, function()
+						action(prompt_bufnr)
+					end)
+				end
+				return true
+			end,
+		})
+		:find()
 end
 return M
